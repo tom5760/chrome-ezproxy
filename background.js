f6dd97c51e1bf34c7058ea34f358204bc7995b5e
Tom Wambold <tom5760@gmail.com>
2012-07-03 23:02:50 -0400

    Update to manifest version 2.

---
+++ background.js
@@ -0,0 +1,25 @@
+const DEFAULT_BASE_URL = "http://www.library.drexel.edu/cgi-bin/r.cgi?url=$@";
+
+function transformUrl(url) {
+    var base = localStorage["base_url"];
+    if (!base) {
+        base = localStorage["base_url"] = DEFAULT_BASE_URL;
+    }
+
+    if (base.indexOf("$@") >= 0) {
+        return base.replace("$@", url);
+    }
+    return base;
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.update(tab.id, {"url": transformUrl(tab.url)});
+});
+
+chrome.contextMenus.create({
+    "title": "Open Link with EZProxy",
+    "contexts": ["link"],
+    "onclick": function(info, tab) {
+        chrome.tabs.create({"url": transformUrl(info.linkUrl)});
+    }
+});
