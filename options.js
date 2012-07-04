068c02eeee6b612aa0a8793c65a09908dc69d906
Tom Wambold <tom5760@gmail.com>
2012-07-03 23:25:18 -0400

    Fix problem with options page and Chrome's content security policy.
    http://code.google.com/chrome/extensions/contentSecurityPolicy.html#H3-1

---
+++ options.js
@@ -0,0 +1,89 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var status_field;
+    var url_box;
+    var select_box;
+    var select_radio;
+    var custom_radio;
+
+    function init_options() {
+        status_field = document.getElementById("status");
+        url_box = document.getElementById("base_url_box");
+        select_box = document.getElementById("base_url_select");
+        select_radio = document.getElementById("url_select");
+        custom_radio = document.getElementById("url_custom");
+        save_button = document.getElementById("save_button");
+
+        select_radio.addEventListener("click", radio_onclick);
+        custom_radio.addEventListener("click", radio_onclick);
+        select_box.addEventListener("change", select_onchange);
+        save_button.addEventListener("click", save_options);
+
+        update_url_select();
+        restore_options();
+    }
+
+    function save_options() {
+        localStorage["base_url"] = url_box.value;
+
+        // Update status to let user know options were saved.
+        status_field.innerHTML = "Options Saved.";
+        setTimeout(function() {
+            status_field.innerHTML = "";
+        }, 2000);
+    }
+
+    function restore_options() {
+        var base_url = localStorage["base_url"];
+
+        if (base_url) {
+            url_box.value = base_url;
+            select_box.value = base_url;
+            if (select_box.value == base_url) {
+                select_radio.checked = true;
+            } else {
+                custom_radio.checked = true;
+            }
+        } else {
+            select_radio.checked = true;
+            select_box.selectedIndex = 0;
+        }
+        radio_onclick();
+        select_onchange();
+    }
+
+    function select_onchange() {
+        if (select_radio.checked) {
+            url_box.value = select_box.value;
+        }
+    }
+
+    function radio_onclick() {
+        select_box.disabled = !select_radio.checked;
+        url_box.disabled = select_radio.checked;
+    }
+
+    function update_url_select() {
+        var req = new XMLHttpRequest();
+        req.open("GET",
+                "http://ezproxy-db.appspot.com/proxies.json", false);
+        req.onreadystatechange = function(event) {
+            if (req.readyState != 4 || req.status != 200) {
+                return;
+            }
+            var response = JSON.parse(req.responseText);
+            for (var i = select_box.length - 1; i >= 0; i--) {
+                select_box.remove(i);
+            }
+            for (var i in response.proxies) {
+                proxy = response.proxies[i];
+                var option = document.createElement("option");
+                option.text = proxy.name;
+                option.value = proxy.url;
+                select_box.add(option, null);
+            }
+        }
+        req.send();
+    }
+
+    init_options();
+});
